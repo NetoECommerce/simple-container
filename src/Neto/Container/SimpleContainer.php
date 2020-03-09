dddfe6c407b915134c8a0cb8fc6dca2e00071e47
Ben Plunkett <ben.plunkett@neto.com.au>
2020-03-09 16:03:37 +1000

    Changed $id to $identifier to appease the PHPMD gods

--- src/Neto/Container/SimpleContainer.php
+++ src/Neto/Container/SimpleContainer.php
@@ -23,45 +23,45 @@ class SimpleContainer implements ContainerInterface
     /**
      * Adds an entry to the container with an identifier.
      *
-     * @param string $id
+     * @param string $identifier
      * @param mixed $value
      *
      * @return void
      */
-    public function add($id, $value)
+    public function add($identifier, $value)
     {
-        $this->container[$id] = $value;
+        $this->container[$identifier] = $value;
     }
 
     /**
      * Finds an entry of the container by its identifier and returns it.
      *
-     * @param string $id Identifier of the entry to look for.
+     * @param string $identifier Identifier of the entry to look for.
      *
      * @throws NotFoundExceptionInterface  No entry was found for this identifier.
      * @throws ContainerExceptionInterface Error while retrieving the entry.
      *
      * @return mixed Entry.
      */
-    public function get($id)
+    public function get($identifier)
     {
-        if ($this->has($id)) {
-            return $this->container[$id];
+        if ($this->has($identifier)) {
+            return $this->container[$identifier];
         }
 
-        throw new NotFoundException(sprintf('Could not find container definition for %s', $id));
+        throw new NotFoundException(sprintf('Could not find container definition for %s', $identifier));
     }
 
     /**
      * Returns true if the container can return an entry for the given identifier.
      * Returns false otherwise.
      *
-     * @param string $id Identifier of the entry to look for.
+     * @param string $identifier Identifier of the entry to look for.
      *
      * @return bool
      */
-    public function has($id)
+    public function has($identifier)
     {
-        return isset($this->container[$id]);
+        return isset($this->container[$identifier]);
     }
 }
